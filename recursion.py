c5aaed6c23d007134e25180019c8911199f5f0b7
杨超 <1659514328@qq.com>
2014-12-22 20:06:23 +0800

    m

--- recursion.py
+++ recursion.py
@@ -6,4 +6,14 @@ def fact(n):
 	if n == 1:
 		return 1
 	return n * fact(n - 1)
-print fact(100)
+print fact(1000)
+
+def fact(n):
+	return fact_iter(1, 1, n)
+
+def fact_iter(product, count, max):
+	if count > max:
+		return product
+	return fact_iter(product + count, count + 1, max)
+	
+print fact(50)
