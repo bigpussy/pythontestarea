dd6fe9658ecf84524e73d434b5ba772f2be5a985
杨超 <1659514328@qq.com>
2014-12-24 11:22:37 +0800

    python

---
+++ Partialfunc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+print u"偏函数"
+print int('12345')
+print u"可传入base参数"
+print int('1111', base = 2)
+print u"我们可以写一个只装换2进制的函数"
+def int2(x , base = 2):
+	return int(x , base)
+print int2('100000')
+print u"functools.partial就是帮助我们穿件一个偏函数的"
+import functools
+int4 = functools.partial(int , base = 5)
+print int4('12')
--- decorator.py
+++ decorator.py
@@ -6,9 +6,42 @@ f = now
 f()
 print now.__name__
 print f.__name__
-
+print u"decorator"
 def log(func):
 	def wrapper(*args, **kw):
 		print 'call %s():' % func.__name__
 		return func(*args, **kw)
 	return wrapper
+
+@log
+def now():
+	print '2013-12-25'
+now()
+print u"传送参数"
+import functools
+def log(text='decorator'):
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+			print '%s %s():' %(text, func.__name__)
+			return func(*args, **kw)
+		return wrapper
+	return decorator
+@log('in the name of')
+def now():
+	print '2013-12-25'
+now()
+print now.__name__
+
+print u"练习1"
+def log(func):
+	@functools.wraps(func)
+	def decorator(*args, **kw):
+		print 'begin call'
+		func(*args, **kw)
+		print 'end call'
+	return decorator
+@log
+def now():
+	print '20151515'
+now()
