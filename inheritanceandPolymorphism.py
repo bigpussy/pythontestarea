04d28fa5c3a49378d595f06043c103ae4f48ab13
杨超 <1659514328@qq.com>
2014-12-24 18:21:11 +0800

    change file

---
+++ inheritanceandPolymorphism.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+print u"继承和多态"
+
+class Animal(object):
+	def run(self):
+		print 'Animal is running...'
+class Dog(Animal):
+	def run(self):
+		print 'Dog is running...'
+class Cat(Animal):
+	def run(self):
+		print 'Cat is running...'
+
+dog = Dog()
+cat = Cat()
+
+dog.run()
+cat.run()
+
+a = list()
+
+b = Animal()
+
+c = Dog()
+
+print isinstance(a, list)
+print isinstance(b, Animal)
+print isinstance(c, Dog)
+print isinstance(c, Animal)
+class Tortoise(object):
+	def run(self):
+		print "Tortoise is running"
+
+def run_twice(animal):
+	animal.run()
+	animal.run()
+run_twice(Tortoise())
+
