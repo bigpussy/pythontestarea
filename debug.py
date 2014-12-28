573af58dc8992e5252707d17686ae987226323e2
杨超 <1659514328@qq.com>
2014-12-28 14:50:59 +0800

    test

--- debug.py
+++ debug.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
-print u"调试"
-def foo(s):
-	n = int(s)
-	print '>>> n = %d' % n 
-	assert n != 0, 'n is zero'
-	return
-def main():
-	foo('1')
-
-
-main()
-
+import pdb
+s = '1'
+n = int(s)
+pdb.set_trace()
+print 10/n
 
