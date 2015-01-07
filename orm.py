6006762029288191a275146108ad513927ccf358
杨超 <1659514328@qq.com>
2015-01-07 16:57:34 +0800

    Signed-off-by: 杨超 <1659514328@qq.com>

--- orm.py
+++ orm.py
@@ -15,3 +15,11 @@ class User(Base):
 engine = create_engine('mysql+mysqlconnector://root:password@192.168.42.129:3306/test')
 
 DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+user = session.query(User).filter(User.id=='1').one()
+
+print 'type:' , type(user)
+print 'name:', user.name 
+session.close()
