9531ebe76c8339a413962bc4ebdc1e096645c8ef
杨超 <1659514328@qq.com>
2015-01-07 14:22:44 +0800

    Signed-off-by: 杨超 <1659514328@qq.com>

---
+++ orm.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+	
+	__tablename__ = 'user'
+	
+	id = Column(String(20), primary_key=True)
+	name = Column(String(20))
+
+engine = create_engine('mysql+mysqlconnector://root:password@192.168.42.129:3306/test')
+
+DBSession = sessionmaker(bind=engine)
