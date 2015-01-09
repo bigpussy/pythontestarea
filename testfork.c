df5303f78467abbe91df858100506184f538e40e
yangchao <1659514328@qq.com>
2015-01-09 15:48:15 +0800

    test

--- testfork.c
+++ testfork.c
@@ -5,4 +5,23 @@
 
 int main (int argc, char *argv[]){
 	pid_t pid;
+	int cnt = 0;
+
+	pid = fork();
+	pid = fork();
+
+	printf("pid  is %d\n", pid);
+//
+//	if(pid == -1){
+//		perror("fork error");
+//	}else if(pid == 0){
+//		printf("The returned value is %d\nIn child process!!\nMy PID is %d\n", pid, getpid());
+//		cnt ++;
+//	}else{
+//		printf("The returned value is %d\nIn father process!!\nMy PID is %d\n", pid, getpid());
+//		cnt ++;
+//	}
+//	printf("cnt = %d\n", cnt);
+
+	return 0;
 }
